2562ad729cb04554104090e235d108123c3966c2
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-11-29 11:30:28 +0100

    Start components
    Start components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    LoginComponent,
+    AboutComponent,
+    ShoppingcartComponent
   ],
   imports: [
     BrowserModule,
