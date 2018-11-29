a511a94020de9df22fc32c8c3f71e2cb0fe5135a
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-11-29 12:03:31 +0100

    navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent} from "./welcome/welcome.component";
+import { ProductsComponent} from "./products/products.component";
+import { ProductDetailComponent} from "./product-detail/product-detail.component";
+import { ShoppingcartComponent} from "./shoppingcart/shoppingcart.component";
+import { LoginComponent} from "./login/login.component";
+import { AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent},
+  { path: 'product/:id', component: ProductDetailComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'shoppingcart', component: ShoppingcartComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
