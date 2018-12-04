7e972337e606bad4bf3c6384d73bb97e85e11988
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-12-04 10:34:11 +0100

    Authentication login component and auth service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../Services/authentication.service";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  loading = false;
+  errormessage = '';
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    //  Initialize the form group
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // reset login status
+    this.authenticationService.logout();
   }
 
+  // Getters for easy access to form fields
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.login(this.username.value, this.password.value)
+      .subscribe(
+        success => {
+          this.router.navigate(['/']);
+        },
+        error => {
+          this.errormessage = error.message;
+          this.loading = false;
+        });
+  }
 }
