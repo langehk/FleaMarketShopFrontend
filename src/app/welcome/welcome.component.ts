17d6f874bde441bbe2a7751487561c6e9340d00a
langehk <rupkri32@gmail.com>
2018-12-10 12:07:38 +0100

    welcome page + list products

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../shared/Services/product.service';
+import {Product} from '../shared/Models/product';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.refresh();
   }
 
+  refresh() {
+    this.productService.getProducts().subscribe(products => {this.products = products; });
+  }
 }
+
