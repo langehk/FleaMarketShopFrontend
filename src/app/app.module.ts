0ed92505730a4c05205f8f5e7937ba22406107ae
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-12-10 10:48:22 +0100

    dropdown bootstrap module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AddCategoryComponent } from './category/add-category/add-category.compo
 import { UpdateCategoryComponent } from './category/update-category/update-category.component';
 import { ListProductsComponent } from './product/list-products/list-products.component';
 import { ListCategoriesComponent } from './category/list-categories/list-categories.component';
+import { SelectDropDownModule } from 'ngx-select-dropdown'
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { ListCategoriesComponent } from './category/list-categories/list-categor
     AppRoutingModule,
     HttpClientModule,
     ButtonsModule.forRoot(),
-    CarouselModule, FormsModule
+    CarouselModule, FormsModule,
+    SelectDropDownModule
   ],
   providers: [AuthGuard,
     AuthenticationService,
