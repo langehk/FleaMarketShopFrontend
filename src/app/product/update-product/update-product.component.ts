fcbc598fae6627f25ece4acbe04b0fd00eb872a7
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-12-11 18:21:50 +0100

    working on update not working yet

--- src/app/product/update-product/update-product.component.ts
+++ src/app/product/update-product/update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ProductService} from "../../shared/Services/product.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-update-product',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProductComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+
+  updateProductForm = new FormGroup( {
+    productName: new FormControl(''),
+    productPrice: new FormControl(''),
+    productDescription: new FormControl(''),
+    mainPictureString: new FormControl(''),
+    category: new FormGroup({
+      categoryId: new FormControl('')})
+  });
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(this.id).
+    subscribe(product => {
+      this.updateProductForm.patchValue({
+        productName: product.productName,
+        productPrice: product.productPrice,
+        productDescription: product.productDescription,
+        mainPictureString: product.mainPictureString,
+        product: new FormGroup({
+          Product: new FormControl('')})
+      });
+    });
   }
 
+  save(){
+    const product = this.updateProductForm.value;
+    product.productId = this.id;
+    this.productService.updateProduct(product)
+      .subscribe(() => {
+        this.router.navigateByUrl('/admin');
+      });
+  }
 }
